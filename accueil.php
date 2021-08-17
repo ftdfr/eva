27ec617d20f24e423b7c0c9316b91d2451733f70
Félix <77358382+ftdfr@users.noreply.github.com>
2021-08-17 17:03:56 +0200

    Add files via upload

---
+++ accueil.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="CSS/style.css" />
+    <link rel="icon" type="image/png" sizes="16x16" href="Pictures/EAGV.png">
+    <title>Accueil</title>
+  </head>
+
+  <body>
+    <div class="menu">
+      <?php include 'includes/nav.php'; ?>
+    </div>
+    <header id="accueil">    
+       <h1 id="Eva">Accueil</h1>
+    </header>
+  </body>
+</html>
